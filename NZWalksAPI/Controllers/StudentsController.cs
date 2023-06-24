782ba73edaf2ba9cbd63b682b61311feda8bb459
Ömer Faruk Ayyıldız <omerfaruk914@gmail.com>
2023-06-24 14:22:22 +0300

    Staged commit

--- NZWalksAPI/Controllers/StudentsController.cs
+++ NZWalksAPI/Controllers/StudentsController.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace NZWalksAPI.Controllers
@@ -11,5 +6,12 @@ namespace NZWalksAPI.Controllers
     [ApiController]
     public class StudentsController : ControllerBase
     {
+        
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentNames = { "Ömer Faruk", "Ayşe", "Remzi", "Tarık", "Sümeyye", "Sena", "Merve", "Mehmet" };
+            return Ok(studentNames);
+        }
     }
 }
--- NZWalksAPI/Data/NzWalksDbContext.cs
+++ NZWalksAPI/Data/NzWalksDbContext.cs
@@ -1,6 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using NZWalksAPI.Models.Domain;
+
 namespace NZWalksAPI.Data;
 
-public class NzWalksDbContext
+public class NzWalksDbContext : DbContext
 {
-    
+    public NzWalksDbContext(DbContextOptions dbContext) : base(dbContext)
+    {
+        
+    }
+
+    public DbSet<Region> Regions { get; set; }
+    public DbSet<Difficulty> Difficulties { get; set; }
+    public DbSet<Walk> Walks { get; set; }
 }
