89776248b3395365b0ebaebcfa2406dd5fba35b5
Ömer Faruk Ayyıldız <omerfaruk914@gmail.com>
2023-10-21 14:20:50 +0300

    Jwt key generator codes added. Config completed.

---
+++ NZWalksAPI/Repositories/ITokenRepository.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace NZWalksAPI.Repositories;
+
+public interface ITokenRepository
+{
+    string CreateJWTToken(IdentityUser user, List<string> Roles);
+}
